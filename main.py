570b7859f0d8796b72902e601a98694ffbefbbe0
ishansrivastava <ishansrivastava106@gmail.com>
2020-09-08 23:02:34 +0530

    added timer

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# This is a sample Python script.
 import math
 import random
 import sys
+import time
+import threading
 from collections import deque
 
 
@@ -49,11 +50,14 @@ def distanceFromFood(row, column):
     else:
         return (math.sqrt((pow(row - foodRow, 2) + pow(column - foodColumn, 2))))
 
+def timerCalc():
+    while(true):
+        displayMatrix()
+        print("Enter Input:")
+        global x = input()
+        time.sleep(1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    createSnakeMatrix()
-    displayMatrix()
+def snakeMoves():
     currentRow = snakeRow
     currentColumn = snakeColumn
     llist.append(Coordinates(snakeRow, snakeColumn))
@@ -62,8 +66,7 @@ if __name__ == '__main__':
         if len(llist) == rows * columns:
             print("Congratulations you won!!")
             break;
-        print("Enter input")
-        move = input()
+        move = x
         if move == 'w':
             if (llist[-1].x - 1) < 0 or snakeMatrix[llist[-1].x - 1][llist[-1].y] == '+':
                 print("Dead...")
@@ -72,7 +75,7 @@ if __name__ == '__main__':
                 snakeMatrix[llist[-1].x - 1][llist[-1].y] = "+"
                 displayMatrix()
                 llist.append(Coordinates(llist[-1].x - 1, llist[-1].y))
-                food(int(random.random()*10),int(random.random()*10))
+                food(int(random.random() * 10), int(random.random() * 10))
                 displayMatrix()
             else:
                 snakeMatrix[llist[-1].x - 1][llist[-1].y] = "+"
@@ -88,7 +91,7 @@ if __name__ == '__main__':
                 snakeMatrix[llist[-1].x + 1][llist[-1].y] = '+'
                 displayMatrix()
                 llist.append(Coordinates(llist[-1].x + 1, llist[-1].y))
-                food(int(random.random()*10),int(random.random()*10))
+                food(int(random.random() * 10), int(random.random() * 10))
                 displayMatrix()
             else:
                 snakeMatrix[llist[-1].x + 1][llist[-1].y] = '+'
@@ -104,7 +107,7 @@ if __name__ == '__main__':
                 snakeMatrix[llist[-1].x][llist[-1].y - 1] = '+'
                 displayMatrix()
                 llist.append(Coordinates(llist[-1].x, llist[-1].y - 1))
-                food(int(random.random()*10),int(random.random()*10))
+                food(int(random.random() * 10), int(random.random() * 10))
                 displayMatrix()
             else:
                 snakeMatrix[llist[-1].x][llist[-1].y - 1] = '+'
@@ -120,7 +123,7 @@ if __name__ == '__main__':
                 snakeMatrix[llist[-1].x][llist[-1].y + 1] = '+'
                 displayMatrix()
                 llist.append(Coordinates(llist[-1].x, llist[-1].y + 1))
-                food(int(random.random()*10),int(random.random()*10))
+                food(int(random.random() * 10), int(random.random() * 10))
                 displayMatrix()
             else:
                 snakeMatrix[llist[-1].x][llist[-1].y + 1] = '+'
@@ -131,6 +134,14 @@ if __name__ == '__main__':
         else:
             print("Wrong Input Please press right button\n")
         print("snake length:", len(llist))
+
+if __name__ == '__main__':
+    createSnakeMatrix()
+    thread.start_new_thread(timerCalc, ())
+    thread.start_new_thread(snakeMoves,())
+
+
+
         # displayMatrix()
         # for i in range(0,len(llist)):
         # print("(",llist[i].x,",",llist[i].y,")",",")
